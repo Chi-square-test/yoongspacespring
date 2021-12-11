5015ef4e7bfa4729da3a9a574a5ad8d2aec1da66
Chi-square-test <luckykr1234@gmail.com>
2021-12-11 17:11:35 +0900

    접속자 정보 추가

---
+++ demo/src/main/java/com/yoongspace/demo/DTO/UserInfoDTO.java
@@ -0,0 +1,16 @@
+package com.yoongspace.demo.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfoDTO {
+    private String username;
+    private String userinfo;
+
+}

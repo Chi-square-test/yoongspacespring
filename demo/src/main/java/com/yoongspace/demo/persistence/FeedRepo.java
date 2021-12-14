cc3ec2e044ee258dd0382fb92dffe40d294600c4
Chi-square-test <luckykr1234@gmail.com>
2021-12-14 20:13:49 +0900

    팁,팀 카테고리 게시글을 확인할 수 있는 get 엔드포인트 추가, 다만 글 작성시 아직은 자유게시판을 반환하니 주의, 추후 코드 수정 예정

--- demo/src/main/java/com/yoongspace/demo/persistence/FeedRepo.java
+++ demo/src/main/java/com/yoongspace/demo/persistence/FeedRepo.java
@@ -9,8 +9,9 @@ import java.util.Optional;
 public interface FeedRepo extends JpaRepository<FeedEntity, String> {
     int countByIdAndStudentid(Long id,String studentid);
     List<FeedEntity> findByOnlyfriend(boolean onlyfriend);
-    List<FeedEntity> findByOnlyfriendAndStudentid(boolean onlyfriend,String studentid);
+    List<FeedEntity> findByOnlyfriendAndStudentid(boolean onlyfriend,String studentid);//주용도 onlyf로 내가 쓴글 찾기
     List<FeedEntity> findByStudentid(String studentid);
+    List<FeedEntity> findByOnlyfriendAndAndCat(boolean onlyfriend,String cat);
 
     Optional<FeedEntity> findById(Long id);
 }
